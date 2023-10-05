e7b985723dad7962d4e70d78449f2bcb2d6aacc3
Vosik <vosikvos@gmail.com>
2023-10-06 00:45:04 +0300

    added animation

---
+++ Netflix Clone/Views/CustomNotitficationUIView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomNotitficationUIView.swift
+//  Netflix Clone
+//
+//  Created by Саша Восколович on 04.10.2023.
+//
+
+import UIKit
+
+class CustomNotitficationUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
